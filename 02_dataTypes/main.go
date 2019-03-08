fcf937d8d2727c6cf30800edd8aa0f4ac9df3b13
Eyder ACM <eyderacm@gmail.com>
2019-03-07 22:30:12 -0600

    Setup starter files

---
+++ 02_dataTypes/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/* It is implied that this is a String, so
+		 writing "string" afterwards, isn't needed
+	var name string = "Eyder"
+	*/
+
+	var name = "Eyder"
+	var age int = 37
+
+	fmt.Println(name, age)
+}
